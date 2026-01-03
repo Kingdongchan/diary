09659e17ff481fc077bd86988f3f425b8815fa44
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 13:47:18 +0900

    사이드바 위, 아래 칸 나누기

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import main_diary as md
 
 root = tk.Tk()
 root.title("Diary")
@@ -7,6 +8,14 @@ root.geometry("800x600")
 #사이드바 형성
 side_frame = tk.Frame(root, width=200, bg="lightgray")
 side_frame.pack(side="left")
+
+# 사이드 바 2공간으로 나누기
+#위에 칸 큰 제목이 들어올 곳
+side_up = tk.Frame(side_frame, width=200, bg="lightgray")
+side_up.pack(side="top")
+#아래칸(버튼이 들어올 곳
+side_down = tk.Frame(side_frame, width=200, bg="lightgray")
+side_down.pack(side="top")
 
 #메인바 형성
 main_frame = tk.Frame(root, width=600, bg="white")
