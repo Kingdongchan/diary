e75280c4bb7398442558de03f4f5bf4f18caa904
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 13:15:06 +0900

    init -> 언더바 하나 삭제, width, height-> 삭제

--- button.py
+++ button.py
@@ -2,17 +2,15 @@ import tkinter as tk
 
 #버튼 클래스 생성
 class button:
-    def __init___(self, main, text, width, height, row, column, sticky, command=None):
+    def __init__(self, main, text, row, column, sticky, command=None):
         self.main = main
         self.text = text
-        self.width = width
-        self.height = height
         self.row = row
         self.column = column
         self.command = command
         self.stikcy = sticky
         
     def bnt_maker(self):
-        bnt = tk.Button(self.main, text=self.text, width=self.width, height=self.height, command=self.command)
+        bnt = tk.Button(self.main, text=self.text, command=self.command)
         bnt.grid(row=self.row, column=self.column, sticky=self.stikcy)
         
