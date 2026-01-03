07b2b7bbbaa3e2cce19da8128ed7bbe479258490
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 14:32:39 +0900

    저장을 누르면 왼쪽 사이드바에 하나씩 쌓기

--- main_diary.py
+++ main_diary.py
@@ -4,8 +4,10 @@ from tkinter import messagebox
 import cls.button as bt
 import cls.label as lb
 import cls.text as tx 
+#save_txt을 담을 공간 생성
+save_blank = []
 
-def diary_main(main_frame):
+def diary_main(main_frame, side_frame):
     # +버튼을 누르면 +버튼이 사라지게끔 하는 로직
     for widget in main_frame.winfo_children():
         widget.destroy
@@ -16,7 +18,8 @@ def diary_main(main_frame):
         c = content_txt.blank.get("1.0", "end-1c")
         
         save_txt = {"제목":t, "내용":c}
-        
+        #save_blank에 save_txt 담기
+        save_blank.append(save_txt)        
         #저장을 누르면 저장 완료 창을 띄우기
         messagebox.showinfo("저장", "저장이 완료되었습니다.")
         # 화면을 비우는 로직 생성)
@@ -38,4 +41,14 @@ def diary_main(main_frame):
     # 저장 버튼 생성
     save = bt.button(main_frame, "저장", 2, 2, "n", msg_save)
     save.bnt_maker()
-   
+    
+    #사이드바에 자신이 쓴 제목을 노출시키는 메인 바가 있어야 함
+    side_frame_title = tk.Frame(side_frame, height=2, bg="gray", bd=1, relief="groove")
+    side_frame.grid(sticky="n")
+    #텍스트담은 곳에 제목을 띄우기
+    for i in range(len(save_blank)):
+        save_blank_type = save_blank[i]["제목"]
+        side_frame_text = tk.Label(side_frame_title, text=save_blank_type, bg="gray")
+        side_frame_text.grid(row=i, column=0, sticky="n")
+        
+        
