bd32918cb5001e5475c24969c629aa18d24baa1e
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 20:38:09 +0900

    사이드바, 메인틀 새로고침 추가

--- delete.py
+++ delete.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox 
 
-def side_del (index, save_blank):
+import main_diary as md
+import reset as rs
+def side_del (index, save_blank, side_up, main_frame):
     #삭제 할 떄 정말 삭제할 지 이야기 해주는 안내창
     del_answer = messagebox.askyesno("삭제", "정말로 삭제하겠습니까?")
     
@@ -9,3 +11,7 @@ def side_del (index, save_blank):
     if del_answer:
         
         del save_blank[index]
+        # 사이드바 새로고침
+        md.side_title(side_up, main_frame)
+        # 메인틀 새로고침
+        rs.re_set(main_frame, side_up)
