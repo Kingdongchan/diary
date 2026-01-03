6de80378e83b533a52d147ad835bdebc07d70e94
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 12:55:19 +0900

    텍스트 클래스 생성

---
+++ text.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+#텍스트 클래스 생성
+class text:
+    def __init___(self, main, width, height, row, column, sticky):
+        self.main = main
+        self.width = width
+        self.height = height
+        self.row = row
+        self.column = column
+        self.stikcy = sticky
+        
+    def bnt_maker(self):
+        txt = tk.Text(self.main, width=self.width, height=self.height)
+        txt.grid(row=self.row, column=self.column, sticky=self.stikcy)
+        
