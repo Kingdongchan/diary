996aa700a7a2ad4ce2a419de56f928ed3785b2ff
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 13:33:10 +0900

    저장버튼 누르면 저장되는 로직만들기

--- main_diary.py
+++ main_diary.py
@@ -1,9 +1,24 @@
 import tkinter as tk
 from tkinter import messagebox
 
-import button as bt
-import label as lb
-import text as tx 
+import cls.button as bt
+import cls.label as lb
+import cls.text as tx 
+
+
+#저장을 누르면 화면을 비우고 배열에 담는 로직
+def msg_save():
+    
+    t = title_txt.get("1.0", "end-1c")
+    c = content_txt.get("1.0", "end-1c")
+    
+    save_txt = {"제목":t, "내용":c}
+    #저장을 누르면 저장 완료 창을 띄우기
+    messagebox.showinfo("저장", "저장이 완료되었습니다.")
+    # 화면을 비우는 로직 생성)
+    for widget in root.winfo_children():
+        widget.destory()
+        
 
 root= tk.Tk()
 # '제목'이라는 단어를 입력시키기
@@ -13,13 +28,12 @@ title.lb_meker()
 title_txt = tx.text(root, 100, 1, 0, 1, "ne")
 title_txt.txt_maker()
 # '내용'이라는 단어를 입력시키기
-content = lb.label(root, "내용", 1, 0, "ne")
-content.lb_meker()
+content_txt = lb.label(root, "내용", 1, 0, "ne")
+content_txt.lb_meker()
 # 내용을 쓸 수 있는 입력칸 생성
 title_txt = tx.text(root, 100, 50, 1, 1, "ne")
 title_txt.txt_maker()
 # 저장 버튼 생성
-save = bt.button(root, "저장", 2, 2, "n")
+save = bt.button(root, "저장", 2, 2, "n", msg_save)
 save.bnt_maker()
-
 root.mainloop()   
