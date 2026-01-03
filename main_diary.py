8581bf64916cd55938813e39afc988a2ba74148c
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 13:50:10 +0900

    root를 main_frame으로 변경

--- main_diary.py
+++ main_diary.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 
+import main 
 import cls.button as bt
 import cls.label as lb
 import cls.text as tx 
@@ -23,18 +24,18 @@ def diary_main():
 
     root= tk.Tk()
     # '제목'이라는 단어를 입력시키기
-    title = lb.label(root, "제목", 0, 0, "ne")
+    title = lb.label(main.main_frame, "제목", 0, 0, "ne")
     title.lb_meker()
     # 제목을  쓸 수 있는 입력칸 생성
-    title_txt = tx.text(root, 100, 1, 0, 1, "ne")
+    title_txt = tx.text(main.main_frame, 100, 1, 0, 1, "ne")
     title_txt.txt_maker()
     # '내용'이라는 단어를 입력시키기
-    content = lb.label(root, "내용", 1, 0, "ne")
+    content = lb.label(main.main_frame, "내용", 1, 0, "ne")
     content.lb_meker()
     # 내용을 쓸 수 있는 입력칸 생성
-    content_txt = tx.text(root, 100, 50, 1, 1, "ne")
+    content_txt = tx.text(main.main_frame, 100, 50, 1, 1, "ne")
     content_txt.txt_maker()
     # 저장 버튼 생성
-    save = bt.button(root, "저장", 2, 2, "n", msg_save)
+    save = bt.button(main.main_frame, "저장", 2, 2, "n", msg_save)
     save.bnt_maker()
    
