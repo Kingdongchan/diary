50a1deb429df3834cd0b548373b43515300f687f
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 13:40:49 +0900

    오타 수정

--- main_diary.py
+++ main_diary.py
@@ -13,6 +13,7 @@ def msg_save():
     c = content_txt.blank.get("1.0", "end-1c")
     
     save_txt = {"제목":t, "내용":c}
+    
     #저장을 누르면 저장 완료 창을 띄우기
     messagebox.showinfo("저장", "저장이 완료되었습니다.")
     # 화면을 비우는 로직 생성)
@@ -28,11 +29,11 @@ title.lb_meker()
 title_txt = tx.text(root, 100, 1, 0, 1, "ne")
 title_txt.txt_maker()
 # '내용'이라는 단어를 입력시키기
-content_txt = lb.label(root, "내용", 1, 0, "ne")
-content_txt.lb_meker()
+content = lb.label(root, "내용", 1, 0, "ne")
+content.lb_meker()
 # 내용을 쓸 수 있는 입력칸 생성
-title_txt = tx.text(root, 100, 50, 1, 1, "ne")
-title_txt.txt_maker()
+content_txt = tx.text(root, 100, 50, 1, 1, "ne")
+content_txt.txt_maker()
 # 저장 버튼 생성
 save = bt.button(root, "저장", 2, 2, "n", msg_save)
 save.bnt_maker()
