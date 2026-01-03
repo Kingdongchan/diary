4c642e582d7e95107b620fa37f8b7af100ed6679
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 15:47:07 +0900

    사이드바 안줄어들게하기

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ root.geometry("1000x720")
 #사이드바 형성
 side_frame = tk.Frame(root, width=100, bg="lightgray")
 side_frame.pack(side="left", fill="y")
+#줄어드는 것을 방지하는 명령어
+side_frame.pack_propagate(False)
 
 # 사이드 바 2공간으로 나누기
-
 #위에 칸 큰 제목이 들어올 곳
 side_up = tk.Frame(side_frame, width=200, bg="lightgray")
 side_up.pack(side="top", fill="both", expand=True)
 
-#아래칸(버튼이 들어올 곳
+#아래칸(버튼이 들어올 곳)
 side_down = tk.Frame(side_frame, width=200, bg="lightgray")
 side_down.pack(side="bottom",fill="x")
 
@@ -24,8 +25,8 @@ del_button = tk.Button(side_down, text="삭제", bg="white")
 del_button.pack()
 
 #메인바 형성
-main_frame = tk.Frame(root, width=600, bg="white")
-main_frame.pack(side="left")
+main_frame = tk.Frame(root, width=100, bg="white")
+main_frame.pack(side="left", fill="both", expand=True)
 
 #메인틀에 +버튼을 누르면 일기를 쓸 수 있도록 하는 로직 필요
 plus_button = tk.Button(main_frame, text="+", width=10, height=5, command=lambda:md.diary_main(main_frame, side_up))
