fc3b238886bbec811edf4c2c82d7b351d3bff3f0
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 13:10:29 +0900

    내용와 입력 칸 생성 완료

--- main_diary.py
+++ main_diary.py
@@ -6,6 +6,17 @@ import label as lb
 import text as tx 
 
 root= tk.Tk()
-
+# '제목'이라는 단어를 입력시키기
+title = lb.label(root, "제목", 0, 0, "ne")
+title.lb_meker()
+# 제목을  쓸 수 있는 입력칸 생성
+title_txt = tx.text(root, 100, 1, 0, 1, "ne")
+title_txt.txt_maker()
+# '내용'이라는 단어를 입력시키기
+content = lb.label(root, "내용", 1, 0, "ne")
+content.lb_meker()
+# 내용을 쓸 수 있는 입력칸 생성
+title_txt = tx.text(root, 100, 500, 1, 1, "ne")
+title_txt.txt_maker()
 
 root.mainloop()   
