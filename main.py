3ac7d84b3d0a3946bfefbb8ecd275efc1af8ee23
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 14:13:30 +0900

    expand 추가

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ side_down.pack(side="top")
 
 #메인바 형성
 main_frame = tk.Frame(root, width=600, bg="white")
-main_frame.pack(side="left", fill="both")
+main_frame.pack(side="left", fill="both", expand=True)
 
 plus_button = tk.Button(main_frame, text="+", width=10, height=5, command=lambda:md.diary_main(main_frame))
 plus_button.place(relx=0.5, rely=0.5, anchor="center")
