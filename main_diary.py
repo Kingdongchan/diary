18545d6a08b3798a7a113b9301900e5b1a407ab9
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 14:03:00 +0900

    +버튼을 누르면 +버튼을 사라지게하는 로직 추가

--- main_diary.py
+++ main_diary.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 from tkinter import messagebox
 
-import main 
 import cls.button as bt
 import cls.label as lb
 import cls.text as tx 
 
-def diary_main():
+def diary_main(main_frame):
+    # +버튼을 누르면 +버튼이 사라지게끔 하는 로직
+    for widget in main_frame.winfo_children():
+        widget.destroy
     #저장을 누르면 화면을 비우고 배열에 담는 로직
     def msg_save():
         
@@ -18,24 +20,24 @@ def diary_main():
         #저장을 누르면 저장 완료 창을 띄우기
         messagebox.showinfo("저장", "저장이 완료되었습니다.")
         # 화면을 비우는 로직 생성)
-        for widget in root.winfo_children():
+        for widget in main_frame.winfo_children():
             widget.destroy()
             
 
     root= tk.Tk()
     # '제목'이라는 단어를 입력시키기
-    title = lb.label(main.main_frame, "제목", 0, 0, "ne")
+    title = lb.label(main_frame, "제목", 0, 0, "ne")
     title.lb_meker()
     # 제목을  쓸 수 있는 입력칸 생성
-    title_txt = tx.text(main.main_frame, 100, 1, 0, 1, "ne")
+    title_txt = tx.text(main_frame, 100, 1, 0, 1, "ne")
     title_txt.txt_maker()
     # '내용'이라는 단어를 입력시키기
-    content = lb.label(main.main_frame, "내용", 1, 0, "ne")
+    content = lb.label(main_frame, "내용", 1, 0, "ne")
     content.lb_meker()
     # 내용을 쓸 수 있는 입력칸 생성
-    content_txt = tx.text(main.main_frame, 100, 50, 1, 1, "ne")
+    content_txt = tx.text(main_frame, 100, 50, 1, 1, "ne")
     content_txt.txt_maker()
     # 저장 버튼 생성
-    save = bt.button(main.main_frame, "저장", 2, 2, "n", msg_save)
+    save = bt.button(main_frame, "저장", 2, 2, "n", msg_save)
     save.bnt_maker()
    
