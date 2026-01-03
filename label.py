3a76f0443e74bd93d98585bd76c7bea928f77d0e
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 12:59:35 +0900

    sticky 추가

--- label.py
+++ label.py
@@ -2,13 +2,14 @@ import tkinter as tk
 
 #라벨 클래스 생성
 class label:
-    def __init__(self, main, text, row, column):
+    def __init__(self, main, text, row, column, sticky):
         self.main = main
         self.text = text
         self.row = row
         self.column = column
+        self.sticky = sticky
         
     def lb_meker(self):
         label = tk.Laberl(self.main, text=self.text)
-        label.grid(row=self.row, column=self.column)
+        label.grid(row=self.row, column=self.column, sticky=self.sticky)
     
