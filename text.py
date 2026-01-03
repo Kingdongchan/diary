070c50d3ca710240abf33a81432c92d5e71a9caa
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 13:10:03 +0900

    init - 언더버 하나 삭제

--- text.py
+++ text.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 #텍스트 클래스 생성
 class text:
-    def __init___(self, main, width, height, row, column, sticky):
+    def __init__(self, main, width, height, row, column, sticky):
         self.main = main
         self.width = width
         self.height = height
@@ -10,7 +10,7 @@ class text:
         self.column = column
         self.stikcy = sticky
         
-    def bnt_maker(self):
+    def txt_maker(self):
         txt = tk.Text(self.main, width=self.width, height=self.height)
         txt.grid(row=self.row, column=self.column, sticky=self.stikcy)
         
