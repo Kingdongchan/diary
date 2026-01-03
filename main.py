791ffc497cf88daa9b9e0532ebe627e27e0a6838
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-03 12:41:37 +0900

    메인, 사이드바 형성

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ root = tk.Tk()
 root.title("Diary")
 root.geometry("800x600")
 
+#사이드바 형성
+side_frame = tk.Frame(root, width=200, bg="lightgray")
+side_frame.pack(side="left")
+
+#메인바 형성
+main_frame = tk.Frame(root, width=600, bg="white")
+side_frame.pack(side="left")
+
+
+
 root.mainloop()
